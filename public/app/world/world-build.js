8636c83d16b6a387a688e31d4f24f488e3e2028f
dfountain99 <jaquezfountain99@gmail.com>
2026-09-14 19:30:09 -0400

    Add canonical WISDO World build metadata

---
+++ public/app/world/world-build.js
@@ -0,0 +1,13 @@
+export const WORLD_VERSION = '3.0.0-cinematic-alpha';
+export const WORLD_BUILD_ID = '2026.09.14.cinematic-v1';
+export const WORLD_RENDERER = 'cinematic-city-v1';
+export const WORLD_CITY_ID = 'arcade-central-01';
+export const WORLD_OPERATOR_ASSET = 'wisdo-default-operator-v1';
+
+export const WORLD_BUILD = Object.freeze({
+  worldVersion: WORLD_VERSION,
+  buildId: WORLD_BUILD_ID,
+  renderer: WORLD_RENDERER,
+  city: WORLD_CITY_ID,
+  operatorAsset: WORLD_OPERATOR_ASSET,
+});
